8b3fe4e7b7a9fb079f5efd773e8fe5b584d5d514
eng_Ayat Muhammed <ayatprogram@gmail.com>
2024-10-06 00:06:41 +0300

    Save untracked files before switching branches

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -15,8 +15,8 @@ import environ
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-# env = environ.Env()
-# environ.Env.read_env(BASE_DIR / '.env')
+env = environ.Env()
+environ.Env.read_env(BASE_DIR / '.env')
 
 
 # Quick-start development settings - unsuitable for production
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = env('SECRET_KEY')
-SECRET_KEY = 'django-insecure-7$f^_zx^-&s7m#o(1&r&-c199_xl1l&cj!2q*=)1to_!2u^4(3'
+SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
