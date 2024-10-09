f36dae4ad1a1a838d1cb1fc238b2c6dace7af2c1
sara kareem <125506311+sarakaree@users.noreply.github.com>
2024-10-10 00:54:31 +0300

    Update urls.py

--- littlelemon/littlelemon/urls.py
+++ littlelemon/littlelemon/urls.py
@@ -19,10 +19,8 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    path('',include('meals.urls')),
+    path('meals/',include('meals.urls')),
     path('orders/',include('orders.urls')),
-
     path ('', include('lemonauth.urls')) 
 
 ]
